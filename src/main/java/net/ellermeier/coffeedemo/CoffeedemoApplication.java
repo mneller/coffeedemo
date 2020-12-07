de039da2e6d6067f2e6173d1a9348cb2afd8444d
mneller <martin@ellermeier.net>
2020-12-07 17:33:00 +0100

    Starter WebSpringBoot

---
+++ src/main/java/net/ellermeier/coffeedemo/CoffeedemoApplication.java
@@ -0,0 +1,12 @@
+package net.ellermeier.coffeedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeedemoApplication.class, args);
+    }
+}
---
+++ src/test/java/net/ellermeier/coffeedemo/CoffeedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ellermeier.coffeedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
